b430d81d074559c778cc4a78b4c7126a555b77e2
Daya Kathir <DayaKathir@gmail.com>
2019-07-06 16:03:08 +0530

    web service

--- src/main/java/pizzaloop/PizzaRepository.java
+++ src/main/java/pizzaloop/PizzaRepository.java
@@ -2,17 +2,7 @@ package pizzaloop;
 
 import org.springframework.data.repository.CrudRepository;
 
-import javax.transaction.Transactional;
-import java.util.List;
-@Transactional
-public interface PizzaRepository extends CrudRepository<PizzaDetails, Integer>{
-    /*
-    * Find pizza by Id
-    */
-    List<PizzaDetails> findByPizzaId(Integer id);
+public interface PizzaRepository extends CrudRepository<pizza_details,Integer> {
 
-    /*
-    * Delete pizza by Id
-    */
-    List<PizzaDetails> deleteByPizzaId(Integer id);
 }
+
